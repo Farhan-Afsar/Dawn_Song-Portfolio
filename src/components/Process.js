bcfba84ba549ac97c5d3d9aad60e5317996d9692
Rashedul Islam <santorasu09@gmail.com>
2022-12-08 13:58:31 +0600

    Extended About Page

--- src/components/Process.js
+++ src/components/Process.js
@@ -16,18 +16,7 @@ const Process = ({ dark }) => {
                 data.map((data, i) => (
                   <li className="wow fadeInUp" data-wow-duration="1s" key={i}>
                     <div className="list_inner">
-                      <div className="icon">
-                        <span>
-                          <img
-                            className="brush"
-                            src={`img/brushes/process${dark ? "/dark" : ""}/${
-                              i + 1
-                            }.png`}
-                            alt="image"
-                          />
-                          {parse(data.icons[dark ? "dark" : "light"])}
-                        </span>
-                      </div>
+                    
                       <div className="title">
                         <h3>{data.title}</h3>
                       </div>
